aecc40a57136c1c280305bfb56f2f3a753a16580
kyr <38265ye@gmail.com>
2026-05-07 18:46:33 +0900

    Feat: 프로젝트 초기 설정 - electron 초기화 및 vue 연결 설정

---
+++ main.js
@@ -0,0 +1,28 @@
+import { app, BrowserWindow } from 'electron'
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800
+    })
+
+    win.loadURL('http://localhost:5173')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+console.log('Electron app is running...')
